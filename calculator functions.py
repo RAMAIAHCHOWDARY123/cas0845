a039509bcd18ecb0bde3939df36d2f5f623d9edb
RAMAIAHCHOWDARY123 <112459757+RAMAIAHCHOWDARY123@users.noreply.github.com>
2022-09-06 11:20:10 +0530

    Add files via upload

---
+++ calculator functions.py
@@ -0,0 +1,13 @@
+select=int(input("select the operation 1,2,3,4:"))
+number_1=int(input("enter the first number:"))
+number_2=int(input("enter the second number:"))
+if select==1:
+    print(number_1,"+",number_2,"=",(number_1+number_2))
+elif  select==2:
+    print(number_1,"-",number_2,"=",(number_1-number_2))
+elif  select==3:
+    print(number_1,"*",number_2,"=",(number_1*number_2))
+elif  select==4:
+    print(number_1,"%",number_2,"=",(number_1%number_2))
+else:
+    print("invalid")
