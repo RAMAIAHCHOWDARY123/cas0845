549946dd3cb482cb8b7dddbbcfdf4612e6c912ec
RAMAIAHCHOWDARY123 <112459757+RAMAIAHCHOWDARY123@users.noreply.github.com>
2022-09-06 13:08:29 +0530

    Add files via upload

---
+++ reverse string.py
@@ -0,0 +1,12 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/SANDEEP REDDY/AppData/Local/Programs/Python/Python310/inverted triangle.py
+enter the no of rows:4
+ * * * *
+  * * *
+   * *
+    *
+
+= RESTART: C:/Users/SANDEEP REDDY/AppData/Local/Programs/Python/Python310/inverted triangle.py
+enter a string:
