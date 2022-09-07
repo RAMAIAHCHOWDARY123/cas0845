78bdd416f1887da9d409d889e9a89fcf9d49ed93
RAMAIAHCHOWDARY123 <112459757+RAMAIAHCHOWDARY123@users.noreply.github.com>
2022-09-07 12:54:11 +0530

    Add files via upload

---
+++ square dollar   pattern.py
@@ -0,0 +1,8 @@
+side = int(input("Please Enter any Side of a Square  : "))
+
+print("Square dollar Pattern") 
+
+for i in range(side):
+    for i in range(side):
+        print('$', end = '  ')
+    print()
